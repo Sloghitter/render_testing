9d40d85edc4d22b09f6f82e108809f26f9fd5378
sathya <sathyaraj.d@zohocorp.com>
2026-02-13 11:13:45 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Render!');
+    res.send('Hey guys, this is sathya here');
 });
 
 app.listen(port, () => {
